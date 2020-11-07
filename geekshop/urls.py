25ac6b51e2984eb124dccb2021d16c14e84e3eca
ChausovDS <61182311+ChausovDS@users.noreply.github.com>
2020-11-07 14:54:36 +0300

    первый урок (#1)
    * lesson_1. URLs as regexp
    
    * lesson_1. Send email to file.

---
+++ geekshop/urls.py
@@ -0,0 +1,19 @@
+from django.conf import settings
+from django.conf.urls import include
+from django.conf.urls.static import static
+from django.urls import re_path
+
+import mainapp.views as mainapp
+
+urlpatterns = [
+    re_path(r"^$", mainapp.main, name="main"),
+    re_path(r"^products/", include("mainapp.urls", namespace="products")),
+    re_path(r"^contact/", mainapp.contact, name="contact"),
+    re_path(r"^auth/", include("authnapp.urls", namespace="auth")),
+    re_path(r"^basket/", include("basketapp.urls", namespace="basket")),
+    re_path(r"^admin/", include("adminapp.urls", namespace="admin")),
+]
+
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
