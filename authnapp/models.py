0688edc364f1ebf2b926e92629f2b24e7d125d4b
Dmitriy Chausov <dchausov@inbox.ru>
2020-11-10 15:34:46 +0300

    lesson_2. Social auth

--- authnapp/models.py
+++ authnapp/models.py
@@ -2,6 +2,8 @@ from datetime import timedelta
 
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 from django.utils.timezone import now
 
 
